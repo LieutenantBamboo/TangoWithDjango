7fdec16a407555609b9c6589fb266748877cd803
LieutenantBamboo <mattconnorday@gmail.com>
2017-02-07 17:50:39 +0000

    Another one done

--- rango/tests.py
+++ rango/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.contrib.staticfiles import finders
 from rango.models import Category
+from django.core.urlresolvers import reverse
+from populate_rango import add_cat
 
 
 # Thanks to Enzo Roiz https://github.com/enzoroiz who made these tests during an internship with us
@@ -25,6 +27,36 @@ class CategoryMethodTests(TestCase):
         cat = Category(name='Random Category String')
         cat.save()
         self.assertEqual(cat.slug, 'random-category-string')
+
+
+class IndexViewTests(TestCase):
+
+
+    def test_index_view_with_no_categories(self):
+        """
+        If no questions exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "There are no categories present.")
+        self.assertQuerysetEqual(response.context['categories'], [])
+
+    def test_index_view_with_categories(self):
+        """
+        Check to make sure that the index has categories displayed
+        """
+        add_cat('test', 1, 1)
+        add_cat('temp', 1, 1)
+        add_cat('tmp', 1, 1)
+        add_cat('tmp test temp', 1, 1)
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "tmp test temp")
+        num_cats = len(response.context['categories'])
+        self.assertEqual(num_cats, 4)
+
+
+# Start of given tests for up to chapter 7 (Old, not needed)
 
 class GeneralTests(TestCase):
     def test_serving_static_files(self):
